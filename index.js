6619fb08ca609c15ec5e4d2dad135024b52ec619
John <codebeast0420@gmail.com>
2024-08-20 06:13:05 -0700

    add sheet function

--- index.js
+++ index.js
@@ -4,7 +4,49 @@ const bodyParser = require("body-parser");
 const axios = require("axios").create({ baseUrl: "" });
 const { v4: uuidv4 } = require('uuid');
 const nodemailer = require('nodemailer');
-require('dotenv').config();
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const { JWT } = require('google-auth-library');
+require('dotenv').config(); // Load environment variables from .env file
+
+async function GetGoogleSheetRows() {
+	try {
+		// Create a new JWT client using your service account credentials
+		const serviceAccountAuth = new JWT({
+			email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+			key: process.env.GOOGLE_PRIVATE_KEY.split(String.raw`\n`).join('\n'),
+			scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+		});
+
+		// Create a new GoogleSpreadsheet instance and authenticate
+		const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID, serviceAccountAuth);
+
+		// Load the document information
+		await doc.loadInfo();
+
+		// Access the first sheet
+		const sheet = doc.sheetsByIndex[1];
+
+		// Get all rows from the sheet
+		const rows = await sheet.getRows(); // This fetches all rows
+		console.log("count", sheet.rowCount);
+
+		// Map the rows into a more usable format (e.g., converting each row to a plain object)
+		// console.log("rows", rows);
+		const rowsData = rows.map((row, index) => {
+			const rowData = {};
+			sheet.headerValues.forEach(header => {
+				rowData[header] = row.get(header);
+			});
+			return rowData;
+		});
+
+		return rowsData;
+	} catch (error) {
+		console.error('Failed to access Google Sheets API:', error);
+		return null;
+	}
+}
+
 
 const app = express();
 app.use(bodyParser.json());
@@ -17,18 +59,18 @@ const url = 'https://api.urlbox.io/v1/render/sync';
 
 app.post("/mail", async (req, res) => {
 	let transporter = nodemailer.createTransport({
-    service: 'gmail',
+		service: 'gmail',
 		auth: {
-      user: 'saintjohn0420@gmail.com',
-      pass: 'npklrlxiwppyfpus',
-    }
-  });
-  let mailOptions = {
-    from: req.body.from,
-    to: 'codebeast0420@gmail.com, hello@codebyedge.com, david.evans@codebyedge.com',
-    subject: 'Contact from codeby edge',
-    text: req.body.text
-  };
+			user: 'saintjohn0420@gmail.com',
+			pass: 'npklrlxiwppyfpus',
+		}
+	});
+	let mailOptions = {
+		from: req.body.from,
+		to: 'codebeast0420@gmail.com, hello@codebyedge.com, david.evans@codebyedge.com',
+		subject: 'Contact from codeby edge',
+		text: req.body.text
+	};
 
 	await transporter.sendMail(mailOptions);
 	res.send('success');
@@ -52,6 +94,12 @@ app.post("/screenshot", async (req, res) => {
 
 app.get("/", (req, res) => {
 	res.send("Hello, Worlds")
+})
+
+app.get("/get-sheet", async (req, res) => {
+	console.log('here');
+	const doc = await GetGoogleSheetRows();
+	res.send(doc);
 })
 
 app.post("/add-to-cart", (req, res) => {
